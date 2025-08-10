9e1425db4695dbab0a034632d9355fd13ec10a7b
Brian Farnier <brian.pro1993@gmail.com>
2025-08-10 13:38:59 +0200

    Salut

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 function displayLists(todolists) {
   //var todolist = { name: "ma todo", todos: ["abc", "azerty"] };
 
-  //var  = [todolist, todolist];
+  //var todolits = [todolist, todolist];
 
   const area = document.querySelector("#area");
   area.innerHTML = "";
@@ -12,57 +12,91 @@ function displayLists(todolists) {
     const divList = document.createElement("div");
     divList.id = "list";
     
-  const divNameList = createDivNameList(y, todolists);
-    
+  
     const divTask = document.createElement("div")
     divTask.id = "listTask";
+    const divNameList = document.createElement("div");
+    divNameList.className = "title-liste";
+    const h3NameList = document.createElement("h3");
+    h3NameList.id = "h3NameList";
+    const inputNameList = document.createElement("input");
+    // règle : l'id doit être unique
+    inputNameList.id = "inputNameList-" + y;  // inputNameList-0 pour la première liste inputNameList-1 pour le seconde liste etc...
+    inputNameList.value = todolists[y].name;
 
-    // fonction createBtnAddTask 
+    h3NameList.appendChild(inputNameList);
+
+    divNameList.appendChild(h3NameList);
+    const divNameBtn = document.createElement("div");
+    divNameBtn.className = "regroup-btn";
+
+    const btnEditList = document.createElement("button");
+    btnEditList.className = "edit";
+    btnEditList.id = "btnEditList-"+y;
+    btnEditList.innerHTML = "✏️";
+    divNameBtn.appendChild(btnEditList);
+
+    const btnSupprList = document.createElement("button");
+    btnSupprList.className = "delete";
+    btnSupprList.id = "btnSupprList-"+y;
+    btnSupprList.innerHTML = "X";
+
+    divNameBtn.appendChild(btnSupprList);
+    divNameList.appendChild(divNameBtn);
 
     const btnAddTask = document.createElement("button")
     btnAddTask.className = "addTask"
-    btnAddTask.id = "btnAddTaskList-"+y;
+    btnAddTask.id = "btnAddTaskList-"+ y;
     btnAddTask.innerHTML = "Ajouter Tâche";
-    /*btnAddTask.add listneer click => add une tache vide
-       qui appelle addEmptyTaskToList(todolists[y])
-       puis
-       displayLists(todos)
-*/
+
+
+    // Bouton add task
+    btnAddTask.addEventListener("click", () => {
+      addEmptyTaskToList(y);
+      displayLists(todos);
+
+    } )
+
     for (let i = 0; i < todolists[y].todos.length; i++) {
       //fonction createInputTask
-      const inputTask = document.createElement("input");
-      inputTask.id = "inputTaskList-"+y+"inputTask-"+i;
-          /* listneer value changed 
-       qui appelle updateTaskToList(todolists[y], todolists[y].todos[i])
 
-       ex event
-       input.addEventListener('change', (event) => {
-    console.log("Valeur finale :", event.target.value);
-  });
-*/
+      const inputTask = document.createElement("input");
+      inputTask.id = "inputTaskList-"+ y +"inputTask-" + i;
+
+
+      inputTask.addEventListener("change", (event) =>{
+           updateTaskToList(y,i,event.target.value);
+      })
+    
+
       const deleteTask = document.createElement("button");
-      deleteTask.className = "delete"
+      deleteTask.className = "delete";
       deleteTask.id = "btnDeleteTaskList-"+y+"inputTask-"+i;
+
+      deleteTask.addEventListener("click", () => { 
+        deleteTaskToList(y);
+
+      })
           /* listneer click 
        qui appelle deleteTaskToList(todolists[y], todolists[y].todos[i])
        displayLists(todos)
 */
-      deleteTask.innerHTML = "X"
+      deleteTask.innerHTML = "X";
       inputTask.value = todolists[y].todos[i];
 
       const divIB = document.createElement("div");
-      divIB.className = "divIB"
+      divIB.className = "divIB";
 
-      divTask.appendChild(divIB)
+      divTask.appendChild(divIB);
       divIB.appendChild(inputTask);
-      divIB.appendChild(deleteTask)
+      divIB.appendChild(deleteTask);
 
     }
 
-    divList.appendChild(divNameList)
+    divList.appendChild(divNameList);
     divList.appendChild(divTask);
 
-    divTask.appendChild(btnAddTask)
+    divTask.appendChild(btnAddTask);
     area.appendChild(divList);
   }
 }
@@ -84,51 +118,28 @@ function addToDoList() {
   const listname = input.value
   const newList = { name: listname, todos: [""] }
   input.value = "";
-  addEmptyTaskToList(newlist)
   todos.push(newList);
   displayLists(todos);
-}
 
-function createDivNameList(y, todolists){
-  const divNameList = document.createElement("div");
-    divNameList.className = "title-liste";
-    const h3NameList = document.createElement("h3");
-    h3NameList.id = "h3NameList";
-    const inputNameList = document.createElement("input");
-    // règle : l'id doit être unique
-    inputNameList.id = "inputNameList-"+y;  // inputNameList-0 pour la première liste inputNameList-1 pour le seconde liste etc...
-    inputNameList.value = todolists[y].name;
-
-    h3NameList.appendChild(inputNameList);
-
-    divNameList.appendChild(h3NameList);
-const divNameBtn = document.createElement("div");
-    divNameBtn.className = "regroup-btn";
-
-    const btnEditList = document.createElement("button");
-    btnEditList.className = "edit";
-    btnEditList.id = "btnEditList-"+y;
-    btnEditList.innerHTML = "✏️";
-    divNameBtn.appendChild(btnEditList);
-
-    const btnSupprList = document.createElement("button");
-    btnSupprList.className = "delete";
-    btnSupprList.id = "btnSupprList-"+y;
-    btnSupprList.innerHTML = "X";
-
-    divNameBtn.appendChild(btnSupprList);
-    divNameList.appendChild(divNameBtn);
-
-  return divNameList;
-}
-
-function addEmptyTaskToList(newlist) {
-  newList.todos.push("");
-}
-function addTaskToList(todlist){
 }
 
 btnAjouter.addEventListener("click", addToDoList);
+
+
+function addEmptyTaskToList(y) {
+  todos[y].todos.push("");
+   }
+
+function updateTaskToList(y,i,newValue){
+todos[y].todos[i] = newValue;
+}
+
+
+function deleteTaskToList(y,i){
+todos[y].todos[i] = "";
+displayLists(todos);
+}
+
 
 
 
